bfaffcba4f009ae8eda24196c6e7f9aa9948d9ed
Eddie <eocampo52@gmail.com>
2025-08-11 16:08:57 -0500

    Made Button its own component using component compositioning

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Star from './Star';
+import Modal from './Modal';
+import Button from './Button';
 
 function Rating({
   heading = 'Rate your experience!',
@@ -8,8 +10,22 @@ function Rating({
 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  const handleSubmit = () => {
+    if (rating > 0) {
+      setSubmitted(true);
+    }
+  };
+
+  // Close modal and reset UI
+  const closeModal = () => {
+    setSubmitted(false);
+    setRating(0);
+    setHover(0);
+  };
 
   return (
     <div className='rating-container'>
@@ -29,6 +45,25 @@ function Rating({
         ))}
       </div>
       {rating > 0 && <p className='feedback'>{feedbackMessages[rating - 1]}</p>}
+
+      {/* <button
+        className='submit-btn'
+        onClick={handleSubmit}
+        disabled={rating === 0}
+      >
+        Submit
+      </button> */}
+
+      <Button
+        disabled={rating === 0}
+        className='submit-btn'
+        onClick={handleSubmit}
+      >
+        Submit
+      </Button>
+
+      {/* Modal */}
+      <Modal isOpen={submitted} onClose={closeModal} rating={rating} />
     </div>
   );
 }
